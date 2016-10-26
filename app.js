59a812003b4c3b483c1ef2868dde5a4d33318927
alieissa <ali.portsudan@gmail.com>
2016-10-26 05:16:06 -0400

    Refactored MPs retrieval. All retrievals return a promise now

--- app.js
+++ app.js
@@ -1,8 +1,51 @@
-var gov_side = d3.select('#gov_side');
-var opp_side = d3.select('#opp_side');
 
-opp_side
-  .selectAll('rect')
-  .enter()
-  .select('rect')
-  .data()
+class FB_Model {
+  constructor(url) {
+    firebase.initializeApp({ databaseURL: url });
+    this.db = firebase.database().ref('/MembersOfParliament')
+  }
+
+  // Retrieve MPs between row 'start' and row 'end'
+  _getMps(start, end) {
+    let mpsArray = [];
+    let mpsRef = this.db.orderByChild('Row').startAt(start).endAt(end);
+
+    return mpsRef
+      .once('value')
+      .then((snapshot) => {
+        let mpsCollection = snapshot.val();
+
+        // Convert huge json object to array
+        for(let fullname in mpsCollection) {
+          mpsArray.push(mpsCollection[fullname]);
+        }
+
+        return mpsArray;
+    });
+  }
+
+  getAllMps() {
+    return this.db.once('value').then((snapshot) => { return snapshot.val()})
+  }
+
+  getOppMps() {
+
+    // Opposition occupies rows 0 to 4
+    return this._getMps("00", "04");
+  }
+
+  getGovMps() {
+
+    // Gov occupies rows 7 to 11
+    return this._getMps("07", "11")
+  }
+}
+
+const mps = new FB_Model("https://houseofcommons-d40a9.firebaseio.com");
+mps.getOppMps().then((oppMps) => {
+  // Show d3 rendered graph here
+});
+
+mps.getGovMps().then((govMps) => {
+  // Show d3 rendered graph here
+});
