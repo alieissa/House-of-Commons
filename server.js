8f58c6c0f6136a2b8c0789d83dc754b1421ff677
alieissa <ali.portsudan@gmail.com>
2016-10-28 16:15:38 -0400

    Get server ready to run on Heroku

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
-var path = require('path');
-var express = require('express');
-var app = express();
-var port = 3000;
+const path = require('path');
+const express = require('express');
+const app = express();
+
+
+app.set('port', process.env.PORT || 8000);
 app.use('/', express.static(__dirname));
 app.use('/lib', express.static(path.join(__dirname, 'bower_components')));
-app.get('/', (req, res, err) => {
-  res.sendFile(path.join(__dirname, 'index.html'))
-});
+// app.use('/images', express.static(path.join(__dirname, 'assets/images')));
+// app.use('/css', express.static(path.join(__dirname, 'assets/css')));
+// app.get('/', (req, res, err) => {
+//   res.sendFile(path.join(__dirname, 'index.html'))
+// });
 
-app.listen(port, () => {
-  console.log(`Listening on ${port}`);
+app.listen(app.get("port"), (err) => {
+  console.log(`Listening on ${app.get("port")}`);
 })
