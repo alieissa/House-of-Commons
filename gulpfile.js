1269f5eb7f7a05779b9c5317e31d0dcaa61910ab
alieissa <ali.portsudan@gmail.com>
2016-10-30 07:35:25 -0400

    Added es6 transpiler

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,34 @@
 'use strict';
 
-const jshint  = require('gulp-jshint');
 const gulp = require('gulp');
+const jshint  = require('gulp-jshint');
+const babelify = require('babelify');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
 
-
-gulp.task('lint', () => {
-    return gulp.src('./*.js')
-    .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('jshint-stylish'));
-});
+gulp.task('es6', es6);
+gulp.task('lint', lint);
 
 gulp.task('watch', () => {
-    gulp.watch('./*.js', ['lint']);
+    gulp.watch('./*.js', ['lint', 'es6']);
 });
-gulp.task('default', ['watch']);
+
+gulp.task('default', ['lint', 'es6', 'watch']);
+
+function lint() {
+    return gulp.src('./*.js')
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter('jshint-stylish'));
+}
+
+function es6() {
+	browserify('./app.js')
+		.transform('babelify', {
+			presets: ['es2015']
+		})
+		.bundle()
+		.pipe(source('app.js'))
+		.pipe(buffer())
+		.pipe(gulp.dest('dist/'));
+}
